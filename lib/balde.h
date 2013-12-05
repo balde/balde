9be4ba2cf87209629ace6049c02f78b10bad7a87
Rafael G. Martins <rafael.martins@titansgroup.com.br>
2013-12-05 16:29:07 -0200

    lib: bootstraping...

---
+++ lib/balde.h
@@ -0,0 +1,14 @@
+/**
+ * balde: A microframework for C based on GLib and bad intentions.
+ * Copyright (C) 2013 Rafael G. Martins <rafael@rafaelmartins.eng.br>
+ *
+ * This program can be distributed under the terms of the LGPL-2 License.
+ * See the file COPYING.
+ */
+
+#ifndef _BALDE_H
+#define _BALDE_H
+
+
+
+#endif /* _BALDE_H */
