6d05a5c7da001b7ea880bd38c808eeae5d9a88c7
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2014-01-01 02:24:57 -0200

    added template example

---
+++ examples/hello-with-template.c
@@ -0,0 +1,21 @@
+#include <balde.h>
+#include "templates/foo.h"
+
+balde_response_t*
+hello(balde_app_t *app, balde_request_t *request)
+{
+    balde_response_t *response = balde_make_response("");
+    balde_response_set_tmpl_var(response, "name", "World");
+    balde_template_foo(response);
+    return response;
+}
+
+
+int
+main(int argc, char **argv)
+{
+    balde_app_t *app = balde_app_init();
+    balde_app_add_url_rule(app, "hello", "/", BALDE_HTTP_GET, hello);
+    balde_app_run(app);
+    balde_app_free(app);
+}
