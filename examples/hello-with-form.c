b92546ae74e0812d4b38552851d8feafb6f45364
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2014-01-02 01:02:04 -0200

    examples/hello-with-form: fix empty value handling, again.

--- examples/hello-with-form.c
+++ examples/hello-with-form.c
@@ -8,7 +8,8 @@ hello(balde_app_t *app, balde_request_t *request)
     balde_response_t *response = balde_make_response("");
     if (request->method == BALDE_HTTP_POST) {
         gchar *name = balde_request_get_form(request, "name");
-        balde_response_set_tmpl_var(response, "name", name[0] != '\0' ? name : "World");
+        balde_response_set_tmpl_var(response, "name",
+            name != NULL && name[0] != '\0' ? name : "World");
         balde_template_hello(response);
     }
     else
@@ -21,7 +22,8 @@ int
 main(int argc, char **argv)
 {
     balde_app_t *app = balde_app_init();
-    balde_app_add_url_rule(app, "hello", "/", BALDE_HTTP_GET | BALDE_HTTP_POST, hello);
+    balde_app_add_url_rule(app, "hello", "/", BALDE_HTTP_GET | BALDE_HTTP_POST,
+        hello);
     balde_app_run(app);
     balde_app_free(app);
 }
