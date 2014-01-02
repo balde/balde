930c23eba292caececdb8315f85fdd2082869198
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2014-01-02 00:33:34 -0200

    added example with POST method :)

---
+++ examples/hello-with-form.c
@@ -0,0 +1,27 @@
+#include <balde.h>
+#include "templates/foo.h"
+#include "templates/form.h"
+
+balde_response_t*
+hello(balde_app_t *app, balde_request_t *request)
+{
+    balde_response_t *response = balde_make_response("");
+    if (request->method == BALDE_HTTP_POST) {
+        gchar *name = balde_request_get_form(request, "name");
+        balde_response_set_tmpl_var(response, "name", name != NULL ? name : "World");
+        balde_template_foo(response);
+    }
+    else
+        balde_template_form(response);
+    return response;
+}
+
+
+int
+main(int argc, char **argv)
+{
+    balde_app_t *app = balde_app_init();
+    balde_app_add_url_rule(app, "hello", "/", BALDE_HTTP_GET | BALDE_HTTP_POST, hello);
+    balde_app_run(app);
+    balde_app_free(app);
+}
