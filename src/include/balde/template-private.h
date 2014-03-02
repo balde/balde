2942a9dce854e35bd05815ddf6278d1fab4b3c64
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2014-03-02 03:15:23 -0300

    moved balde-template-gen.c to libbalde; added tests

---
+++ src/include/balde/template-private.h
@@ -0,0 +1,20 @@
+/*
+ * balde: A microframework for C based on GLib and bad intentions.
+ * Copyright (C) 2013-2014 Rafael G. Martins <rafael@rafaelmartins.eng.br>
+ *
+ * This program can be distributed under the terms of the LGPL-2 License.
+ * See the file COPYING.
+ */
+
+#ifndef _BALDE_TEMPLATE_PRIVATE_H
+#define _BALDE_TEMPLATE_PRIVATE_H
+
+#include <glib.h>
+
+gchar* balde_template_generate_source(const gchar *template_name,
+    const gchar *template_source);
+gchar* balde_template_generate_header(const gchar *template_name);
+gchar* balde_template_get_name(const gchar *template_basename);
+int balde_template_main(int argc, char **argv);
+
+#endif /* _BALDE_TEMPLATE_PRIVATE_H */
