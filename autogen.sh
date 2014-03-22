e82cb4adff8bce24e79ab22d1a441742afb85f0d
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2014-03-22 15:58:00 -0300

    autogen.sh: forcing automake-1.11 everywhere.
    replaced WANT_* env vars, that are gentoo specific, by the AUTOMAKE/ACLOCAL
    variables that are understood by autoreconf itself.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-export WANT_AUTOCONF_2_5="yes"
-export WANT_AUTOMAKE_1_11="yes"
-export WANT_AUTOCONF="2.5"
-export WANT_AUTOMAKE="1.11"
+export AUTOMAKE="$(which automake-1.11)"
+export ACLOCAL="$(which aclocal-1.11)"
 
 autoreconf -W all -i
