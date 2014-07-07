c416fde8232bcc6dc2a8257ec1478f2d6973537a
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2014-07-06 23:17:56 -0300

    jenkins: split test running and dependency installation

--- .jenkins.sh
+++ .jenkins.sh
@@ -3,37 +3,6 @@
 # balde test runner for Jenkins CI.
 #
 
-## Install dependencies
-
-CI_DEP_BUNDLE_VERSION=0.2
-CI_DEP_BUNDLE_DEB="balde-ci-dep-bundle_${CI_DEP_BUNDLE_VERSION}_amd64.deb"
-
-export DEBIAN_FRONTEND=noninteractive
-
-sudo apt-get -y -qq install \
-    wget \
-    build-essential \
-    autoconf \
-    automake1.11 \
-    libtool \
-    pkg-config \
-    valgrind \
-    gcc \
-    clang \
-    libfcgi-dev \
-    shared-mime-info \
-    peg \
-    libxml2-utils
-
-wget \
-    --directory-prefix="${HOME}" \
-    --continue \
-    --quiet \
-    "https://github.com/balde/ci-dep-bundle/releases/download/v${CI_DEP_BUNDLE_VERSION}/${CI_DEP_BUNDLE_DEB}"
-
-sudo dpkg -i "${HOME}/${CI_DEP_BUNDLE_DEB}"
-
-
 ## GLib variables
 GLIB_BASE_DIR="/opt/glib/${GLIB_VERSION}"
 
