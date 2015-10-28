973fca537696aa98730ab84e9198726f0dee582a
Jayson Reis (jaysonsantos) <santosdosreis@gmail.com>
2015-10-28 22:13:55 +0100

    Change valgrind parser message

--- build-aux/parse-valgrind-results.sh
+++ build-aux/parse-valgrind-results.sh
@@ -2,8 +2,8 @@
 cat "$1" | grep '[et]ly lost:' | grep '\([0-9][0-9]\|[1-9]\) bytes' &> /dev/null
 
 if [ "$?" -eq "0" ]; then
-  echo -e '\n\n===================================='
-  echo 'CHECK VALGRIND MEMORY LEAKS ANALYSIS'
-  echo -e '====================================\n\n'
+  echo -e '\n\n============================================'
+  echo '    CHECK VALGRIND MEMORY LEAKS ANALYSIS'
+  echo -e '============================================\n\n'
   exit 1
 fi
