0503a0c2b60eb18382153a13521f9cfac277a3e2
Jayson Reis (jaysonsantos) <santosdosreis@gmail.com>
2015-10-28 21:30:25 +0100

    Add a simple script to parse valgrind output and break build in case of memory leaks

---
+++ build-aux/parse-valgrind-results.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cat "$1" | grep '[et]ly lost:' | grep '\([0-9][0-9]\|[1-9]\) bytes' &> /dev/null
+
+if [ "$?" -eq "0" ]; then
+  echo -e '\n\n=================================================='
+  echo 'CHECK THE VALGRIND MEMORY LEAKS ANALYSIS'
+  echo -e '==================================================\n\n'
+  exit 1
+fi
