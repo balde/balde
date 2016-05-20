3d251f953d3e346820f8f98405db08bc53adfdce
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2016-05-20 23:59:38 +0200

    valgrind: use glib suppressions from cockpit project

--- build-aux/parse-valgrind-results.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cat "$1" | grep '[et]ly lost:' | grep '\([0-9][0-9]\|[1-9]\) bytes' &> /dev/null
-
-if [ "$?" -eq "0" ]; then
-  echo -e '\n\n============================================'
-  echo '    CHECK VALGRIND MEMORY LEAKS ANALYSIS'
-  echo -e '============================================\n\n'
-  exit 1
-fi
