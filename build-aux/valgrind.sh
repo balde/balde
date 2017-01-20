922e4653e4c68446a48dc7d1f5eaaee5d7dfa703
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2017-01-20 23:40:27 +0100

    valgrind: run using external script

---
+++ build-aux/valgrind.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+dir="$(dirname "$(realpath "$0")")"
+
+export G_SLICE=always-malloc
+export G_DEBUG=gc-friendly
+export LC_ALL=C
+
+export TESTS_ENVIRONMENT="
+	${VALGRIND:-valgrind} \
+		--tool=memcheck \
+		--leak-check=full \
+		--leak-resolution=high \
+		--show-leak-kinds=definite \
+		--errors-for-leak-kinds=definite \
+		--num-callers=20 \
+		--error-exitcode=1 \
+		--suppressions=${dir}/glib.supp"
+
+if [[ "${1}" == *.sh ]]; then
+	exec "${@}"
+else
+	exec ${TESTS_ENVIRONMENT} "${@}"
+fi
